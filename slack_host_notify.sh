8988c93ee5fd668e106bfd028fb9fc751e15f173
snickerjp <snickerjp@gmail.com>
2020-06-15 19:20:17 +0900

    load config

--- slack_host_notify.sh
+++ slack_host_notify.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-# Edit your Slack hook URL and footer icon URL
-SLACK_URL=https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX
-FOOTER_ICON=http://env.baarnes.com/Nagios.png
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+if [ -f ${SCRIPT_DIR}/slack_notify_config.sh ]; then
+  # load slack_notify_config.sh
+  . ${SCRIPT_DIR}/slack_notify_config.sh
+else
+  # Edit your Slack hook URL and footer icon URL
+  export SLACK_URL=https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX
+  export FOOTER_ICON=http://env.baarnes.com/Nagios.png
+fi
 
 # Host Notification command example :
 
